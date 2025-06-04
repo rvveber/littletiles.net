b33aa33e286a2523517e1fd9a937d9c5db3b49a1
rvveber <weber@b1-systems.de>
2025-06-04 15:40:50 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Basic Development Nix Shell - run with 'nix develop'";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+        system = "x86_64-linux";
+        pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+        
+        buildInputs = [
+            pkgs.docker
+            pkgs.docker-buildx
+            pkgs.bun
+            pkgs.angular-language-server
+        ];
+        
+        shellHook = ''
+            export COMPOSE_BAKE=true;
+            export DOCKER_BUILDKIT=1;
+
+            docker compose down || true;
+            docker compose up -d --build || true;        
+        '';
+
+    };	
+  };
+}
